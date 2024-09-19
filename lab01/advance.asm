7e88a6bbaf2467912f2d8bfde545fd9c10a1b129
LIU ZHE YOU <810496@email.wlsh.tyc.edu.tw>
2024-09-19 10:46:01 +0800

    fix: fix lab01 advance

--- lab01/advance.asm
+++ lab01/advance.asm
@@ -9,6 +9,7 @@ List p=18f4520
 	CLRF 0x001
 	CLRF 0x002
 	CLRF 0x003
+	CLRF 0x004 ; copy of 0x002
 	CLRF 0x010
 	
     init:
@@ -17,9 +18,14 @@ List p=18f4520
 ;        MOVLW b'01111101'
 ;        MOVWF 0x001
 	
-	MOVLW 0xFF
+;	MOVLW 0xFF
+;        MOVWF 0x000 
+;        MOVLW 0x1E
+;        MOVWF 0x001
+    
+	MOVLW 0xA6
         MOVWF 0x000 
-        MOVLW 0x1E
+        MOVLW 0x79
         MOVWF 0x001
 	
     bitwise_operation:
@@ -35,18 +41,22 @@ List p=18f4520
 	ADDWF 0x002, W
 	MOVWF 0x002
 	
+	; copy 0x002 to 0x004
+	MOVF 0x002, W
+	MOVWF 0x004
+	
     count_loop_init:
         MOVLW 0x08
 	MOVWF 0x010 ; store i in [0x010]
     count_loop:
-	BTFSC 0x002, 0; skip if set
+	BTFSC 0x004, 0; skip if set
 	GOTO count_loop_continue
 	GOTO add_count
     add_count:
 	INCF 0x003
 	GOTO count_loop_continue
     count_loop_continue:
-	RRCF 0x002
+	RRCF 0x004
         DECFSZ 0x010
 	    GOTO count_loop
 
