92cec14522f33c51ecca3ea65bb173e537c36b0f
jason810496 <zhu424.dev@gmail.com>
2024-10-17 00:09:17 +0800

    fix: try fix lab04 advance

--- lab04/advance.asm
+++ lab04/advance.asm
@@ -17,73 +17,79 @@ List p=18f4520
     #define c1 0x020
     #define c2 0x021
     #define c3 0x022
+
+	#define TEMP1 0x030
+	#define TEMP2 0x031
     
     
     cross:
 	; Calculate c1 = a2 * b3 - a3 * b2
-	MOVF a2, W         ; W = a2
+	MOVFF a2, WREG        ; W = a2
 	MOVWF PRODL        ; Load a2 into PRODL
-	MOVF b3, W         ; W = b3
+	MOVFF b3, WREG        ; W = b3
 	MULWF PRODL        ; Multiply a2 * b3 (result in PRODH:PRODL)
-	MOVF PRODL, W      ; W = low byte of a2 * b3
+	MOVFF PRODL, WREG     ; W = low byte of a2 * b3
 	MOVWF TEMP1        ; Store result temporarily (low byte)
-	MOVF PRODH, W      ; W = high byte of a2 * b3
+	MOVFF PRODH, WREG     ; W = high byte of a2 * b3
 	MOVWF TEMP2        ; Store result temporarily (high byte)
 
-	MOVF a3, W         ; W = a3
+	MOVFF a3, WREG        ; W = a3
 	MOVWF PRODL        ; Load a3 into PRODL
-	MOVF b2, W         ; W = b2
+	MOVFF b2, WREG        ; W = b2
 	MULWF PRODL        ; Multiply a3 * b2 (result in PRODH:PRODL)
-	MOVF PRODL, W      ; W = low byte of a3 * b2
-	SUBWF TEMP1, F     ; TEMP1 = a2 * b3 - a3 * b2 (low byte)
-	MOVF PRODH, W      ; W = high byte of a3 * b2
-	SUBWFB TEMP2, F    ; TEMP2 = high byte of the result with carry
+	MOVFF PRODL, WREG     ; W = low byte of a3 * b2
+	SUBWF TEMP1, 1     ; TEMP1 = a2 * b3 - a3 * b2 (low byte)
+	MOVFF PRODH, WREG     ; W = high byte of a3 * b2
+	SUBWFB TEMP2, 1    ; TEMP2 = high byte of the result with carry
 
-	MOVF TEMP1, W      ; W = final result for c1 (low byte)
+	MOVFF TEMP1, WREG     ; W = final result for c1 (low byte)
 	MOVWF c1           ; Store c1 in [0x020]
 
 	; Repeat similar steps for c2 = a3 * b1 - a1 * b3
-	MOVF a3, W
+	MOVFF a3, WREG
 	MOVWF PRODL
-	MOVF b1, W
+	MOVFF b1, WREG
 	MULWF PRODL
-	MOVF PRODL, W
+	MOVFF PRODL, WREG
 	MOVWF TEMP1
-	MOVF PRODH, W
+	MOVFF PRODH, WREG
 	MOVWF TEMP2
 
-	MOVF a1, W
+	MOVFF a1, WREG
 	MOVWF PRODL
-	MOVF b3, W
+	MOVFF b3, WREG
 	MULWF PRODL
-	MOVF PRODL, W
-	SUBWF TEMP1, F
-	MOVF PRODH, W
-	SUBWFB TEMP2, F
+	MOVFF PRODL, WREG
+	SUBWF TEMP1, 1
+	MOVFF PRODH, WREG
+	SUBWFB TEMP2, 1
 
-	MOVF TEMP1, W
+	MOVFF TEMP1, WREG
 	MOVWF c2           ; Store c2 in [0x021]
 
 	; Repeat similar steps for c3 = a1 * b2 - a2 * b1
-	MOVF a1, W
+	MOVFF a1, WREG
 	MOVWF PRODL
-	MOVF b2, W
+	MOVFF b2, WREG
 	MULWF PRODL
-	MOVF PRODL, W
+	MOVFF PRODL, WREG
 	MOVWF TEMP1
-	MOVF PRODH, W
+	MOVFF PRODH, WREG
 	MOVWF TEMP2
 
-	MOVF a2, W
+	MOVFF a2, WREG
 	MOVWF PRODL
-	MOVF b1, W
+	MOVFF b1, WREG
 	MULWF PRODL
-	MOVF PRODL, W
-	SUBWF TEMP1, F
-	MOVF PRODH, W
-	SUBWFB TEMP2, F
+	MOVFF PRODL, WREG
+	SUBWF TEMP1, 1
+	MOVFF PRODH, WREG
+	SUBWFB TEMP2, 1
 
-	MOVF TEMP1, W
+	MOVFF TEMP1, WREG
 	MOVWF c3           ; Store c3 in [0x022]
 
 	RETURN
+
+
+	end
