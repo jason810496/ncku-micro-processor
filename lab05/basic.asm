3dac72eac9301a94e8ce1ed28e29795bf2aed525
jason810496 <zhu424.dev@gmail.com>
2024-10-23 13:56:16 +0800

    fix: lab05/basic.asm

--- lab05/basic.asm
+++ lab05/basic.asm
@@ -1,26 +1,28 @@
 #include "xc.inc"
 GLOBAL _mysqrt
-GLOBAL __mysqrt_loop
-GLOBAL __mysqrt_result
 PSECT mytest,local,class=CODE,reloc=2
 
-_mysqrt
+_mysqrt:
     MOVFF WREG, 0x10 ; n = WREG
     CLRF 0x11 ; i = 0
     ; while i++
     ; if i*i > n then break
 
-    __mysqrt_loop
-        INCF 0x11, 1, 0 ; i++, store in WREG
+    __mysqrt_loop:
+        INCF 0x11 ; i++
+	MOVFF 0x11, WREG
         MOVFF WREG, 0x12 ; temp = i
         ; i*i 
         MULWF 0x12
+	; if PRODH is not 0, goto result
+	TSTFSZ PRODH
+	GOTO __mysqrt_result
         ; if PRODL >= n then break
         MOVFF PRODL, WREG
         ; equal case 
-        CPFSLT 0x10 ; Compare f with WREG, skip if <
+        CPFSGT 0x10 ; Compare f with WREG, skip if f < WREG
             GOTO __mysqrt_result
         GOTO __mysqrt_loop
-    __mysqrt_result
-        MOVFF WREG, 0x01
+    __mysqrt_result:
+	MOVFF 0x11, WREG
         RETURN
