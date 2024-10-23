365c1450cf82443df1e1b3bfd5f707172611c1e5
jason810496 <zhu424.dev@gmail.com>
2024-10-23 21:28:45 +0800

    feat: lab05/bonus.asm

--- lab05/bonus.asm
+++ lab05/bonus.asm
@@ -3,39 +3,83 @@ GLOBAL _multi_signed
 PSECT mytest,local,class=CODE,reloc=2
 
 
-_multi_signed
+_multi_signed:
     ; 8-bit signed integer 'a'
     ; 4-bit signed integer 'b'
     ; The output will be a 16-bit result. The result should be stored in an unsigned integer variable
 
     CLRF 0x05 ; a sign; 0 = positive, 1 = negative
     CLRF 0x10 ; result sign, 0 = positive, 1 = negative
+    
+    __input:
+	MOVFF 0x01, 0x03
+	MOVFF WREG, 0x01
 
-    __check_sign_a
-        MOVFF 0x01, WREG ; move a to WREG
-        BN __mark_a_negative
+    __check_sign_a:
+        BTFSC 0x01, 7
+	GOTO __mark_a_negative
         GOTO __check_sign_b
 
-    __mark_a_negative
+    __mark_a_negative:
+	NEGF 0x01 ; make a = -a
         INCF 0x05
 
-    __check_sign_b
-        MOVFF 0x03, WREG ; move b to WREG
-        BN __mark_b_negative
+    __check_sign_b:
+        BTFSC 0x03, 7
+        GOTO __mark_b_negative
         GOTO __mark_b_positive
 
-    __mark_b_positive
+    __mark_b_positive:
         TSTFSZ 0x05 ; skip if a = 0 ( a is positive )
-        ; result be positive
-        GOTO __multiply
+        GOTO __mark_result_negative
+        GOTO __mark_result_positive
         
 
-    __mark_b_negative
-        TSTFSZ 0x05 ; skip if a = 0 ( a is positive )
-        GOTO __multiply ; a is negative and b is negative, result will be positive
-        INCF 0x10 ; result is negative
-        GOTO __multiply
+    __mark_b_negative:
+	NEGF 0x03 ; make b = -b
     
-    __multiply
-        
+        TSTFSZ 0x05 ; skip if a = 0 ( a is positive )
+        GOTO __mark_result_positive
+	GOTO __mark_result_negative
+	
+    __mark_result_positive:
+	GOTO __multiply
+    
+    __mark_result_negative:
+	INCF 0x10 ; result is negative
+	GOTO __multiply
+    
+    __multiply:
+	MOVFF 0x03, 0x20 ; temp n 
+	MOVFF 0x01, 0x21 ; copy of 0x01
+	DECF 0x20 ; temp n --
+    __multiply_loop:
+	MOVFF 0x21, WREG
+	ADDWF 0x01
+	BTFSC STATUS, 0
+	INCF 0x02
+    __multiply_continue:
+	DECFSZ 0x20, 1, 1
+	GOTO __multiply_loop
+	
+	; check result neg, pos
+	TSTFSZ 0x10
+	    GOTO __toggle_result_negative
+	    GOTO __finish
+	    
+    __toggle_result_negative:
+	COMF 0x01
+	
+	COMF 0x02
+	
+	; increase 0x02 if carry
+	
+	INCF 0x01
+	BTFSC STATUS, 0
+	INCF 0x02
+	
+	
+	
+    __finish:
+	RETURN
 
--- lab05/main.c
+++ lab05/main.c
@@ -7,13 +7,20 @@
 
 
 #include <xc.h>
+// #include <cmath>
 
-extern unsigned char mysqrt(unsigned char char); 
+extern unsigned char mysqrt(unsigned char num); 
+extern unsigned int gcd(unsigned int a,unsigned int b);
+extern unsigned int multi_signed(unsigned char a, unsigned char b);
 
 void main(void) {
 
-    volatile unsigned char result = mysqrt(10);
+//    volatile unsigned char result = mysqrt(255);
 
+//    volatile unsigned int result = gcd(5,30);
+    
+    volatile unsigned int result = multi_signed(50,50);
+    
     while(1);
     return;
 }
