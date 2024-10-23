fcb33d3f33c33a560a9384a0b452ec94c72a830d
jason810496 <zhu424.dev@gmail.com>
2024-10-23 15:17:20 +0800

    fix: lab05/advance.asm

--- lab05/advance.asm
+++ lab05/advance.asm
@@ -1,7 +1,5 @@
 #include "xc.inc"
 GLOBAL _gcd
-GLOBAL __gcd_loop
-GLOBAL __gcd_result
 PSECT mytest,local,class=CODE,reloc=2
 
 _swap macro aa, bb
@@ -11,7 +9,7 @@ _swap macro aa, bb
     ENDM 
 
 
-_gcd
+_gcd:
     ; 0x01 = a 
     ; 0x03 = b 
 
@@ -23,21 +21,25 @@ _gcd
     ; make sure a > b
     MOVFF 0x03, WREG
     CPFSLT 0x01 ; Compare f with WREG, skip if <
+	GOTO __gcd_loop
         _swap 0x01, 0x03
 
-    __gcd_loop
+    __gcd_loop:
         ; a-=b
         MOVFF 0x03, WREG
         SUBWF 0x01 ; a -= b 
         ; if a < b then swap a and b
+	MOVFF 0x03, WREG
         CPFSLT 0x01 ; Compare f with WREG, skip if <
-        _swap 0x01, 0x03
+	    GOTO __gcd_loop_continue
+	    _swap 0x01, 0x03
+    __gcd_loop_continue:
         ; if b == 0 then break
         MOVFF 0x03, WREG
         BZ __gcd_result
         GOTO __gcd_loop
 
-    __gcd_result
+    __gcd_result:
         ; a already in 0x01, NOP
         RETURN
 
