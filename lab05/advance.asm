2a4559cbe6e446b6a7e20306f2bc6897dc9f1bad
jason810496 <zhu424.dev@gmail.com>
2024-10-24 11:48:06 +0800

    fix: lab05/advance.asm

--- lab05/advance.asm
+++ lab05/advance.asm
@@ -2,16 +2,23 @@
 GLOBAL _gcd
 PSECT mytest,local,class=CODE,reloc=2
 
-_swap macro aa, bb
-    MOVFF aa, WREG
-    MOVFF bb, aa
-    MOVFF WREG, bb
-    ENDM 
+_swap macro al, ah, bl, bh
+    MOVFF al, WREG
+    MOVFF bl, al
+    MOVFF WREG, bl
+    
+    MOVFF ah, WREG
+    MOVFF bh, ah
+    MOVFF WREG, bh
+    
+    ENDM
 
 
 _gcd:
-    ; 0x01 = a 
-    ; 0x03 = b 
+    ; 0x02 = ah
+    ; 0x01 = al
+    ; 0x04 = bh
+    ; 0x03 = bl
 
     ; assume a > b 
     ; while b != 0
@@ -19,25 +26,64 @@ _gcd:
     ; if a < b then swap a and b
 
     ; make sure a > b
-    MOVFF 0x03, WREG
-    CPFSLT 0x01 ; Compare f with WREG, skip if <
+    
+    __gcd_pre_check_high:
+    MOVFF 0x04, WREG
+    CPFSLT 0x02 ; Compare f with WREG, skip if f < w
+	GOTO __gcd_pre_check_eq
+        _swap 0x01, 0x02, 0x03, 0x04
 	GOTO __gcd_loop
-        _swap 0x01, 0x03
+	
+    __gcd_pre_check_eq:
+	MOVFF 0x04, WREG
+	CPFSEQ 0x02
+	    GOTO __gcd_loop
+	    GOTO __gcd_pre_check_low
+    
+    __gcd_pre_check_low:
+    MOVFF 0x03, WREG
+    CPFSLT 0x01 ; Compare f with WREG, skip if f < w
+	GOTO __gcd_loop
+        _swap 0x01, 0x02, 0x03, 0x04
 
     __gcd_loop:
-        ; a-=b
-        MOVFF 0x03, WREG
-        SUBWF 0x01 ; a -= b 
-        ; if a < b then swap a and b
+	MOVFF 0x04, WREG
+	SUBWF 0x02, 1 ; a_h = a_h - b_h
 	MOVFF 0x03, WREG
-        CPFSLT 0x01 ; Compare f with WREG, skip if <
+	SUBWF 0x01, 1 ; a_l = a_l - b_l
+	BTFSS STATUS, 0 ; If Carry is set -> No borrow. Then skip.
+	DECF 0x02
+	
+	CLRF STATUS
+	
+        ; if a < b then swap a and b
+	__gcd_check_high:
+	MOVFF 0x04, WREG
+	CPFSLT 0x02 ; Compare f with WREG, skip if f < W
+	    GOTO __gcd_check_eq
+	    _swap 0x01, 0x02, 0x03, 0x04
 	    GOTO __gcd_loop_continue
-	    _swap 0x01, 0x03
+	    
+	__gcd_check_eq:
+	MOVFF 0x04, WREG
+	CPFSEQ 0x02
+	    GOTO __gcd_loop_continue
+	    GOTO __gcd_check_low
+	    
+	    
+	__gcd_check_low:
+	MOVFF 0x03, WREG
+        CPFSLT 0x01 ; Compare f with WREG, skip if f < W
+	    GOTO __gcd_loop_continue
+	    _swap 0x01, 0x02, 0x03, 0x04
     __gcd_loop_continue:
         ; if b == 0 then break
-        MOVFF 0x03, WREG
-        BZ __gcd_result
-        GOTO __gcd_loop
+        TSTFSZ 0x03 ; 
+	GOTO __gcd_loop
+	
+	
+	TSTFSZ 0x04
+	GOTO __gcd_loop
 
     __gcd_result:
         ; a already in 0x01, NOP
