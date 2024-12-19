30d32e33ec5412ba5627363717d16707de43e6f2
iting0321 <137410100+iting0321@users.noreply.github.com>
2024-12-19 11:17:48 +0800

    Fix main.c interrupt

--- lab10/bonus/main.c
+++ lab10/bonus/main.c
@@ -3,6 +3,7 @@
 #include "stdio.h"
 #include "string.h"
 // using namespace std;
+#define _XTAL_FREQ 1000000   // Fosc = 1 MHz
 
 char str[20];
 void Mode1(){   // Todo : Mode1 
@@ -18,22 +19,59 @@ void main(void)
     
     while(1) {
         strcpy(str, GetString()); // TODO : GetString() in uart.c
-        if(str[0]=='m' && str[1]=='1'){ // Mode1
-            Mode1();
-            ClearBuffer();
-        }
-        else if(str[0]=='m' && str[1]=='2'){ // Mode2
-            Mode2();
-            ClearBuffer();  
-        }
-    }
-    return;
-}
+        
+        
+        
+        if( stop > 0 ){
+            
+            int prev_stop = stop;
+            LATD = 0;
+            while( prev_stop == stop ){
+                // should #define _XTAL_FREQ 1000000   // Fosc = 1 MHz
+                // before using __delay_ms
+                __delay_ms(500); // 0.5 sec
+                if( LATD == prev_stop ){
+                    LATD = 0;
+                }
+                LATD++;
+            }
+            LATD = 0;
 
-void __interrupt(high_priority) Hi_ISR(void)
-{
-    if(INTCONbits.INT0IF){
-        LATD = 0;
+        }
+        
     }
     return;
 }
+//#pragma interrupt ISR
+//void ISR(void)
+//{
+//    // handle button interrupt
+//    if(INTCONbits.INT0IF){
+//        LATD = 0;
+//        counter = 0;
+//        INTCONbits.INT0IF = 0;
+//    }
+//    
+//    // handle uart interrupt
+//    if(RCIF)
+//    {
+//        if(RCSTAbits.OERR)
+//        {
+//            CREN = 0;
+//            Nop();
+//            CREN = 1;
+//        }
+//        
+//        
+//        MyusartRead();
+//        
+//        // set light to 0
+//        LATD = 0;
+//        counter = 0;
+//        RCIF = 0;   //Clear flag bit
+//        return;
+//    }
+//    
+//    
+//    return;
+//}
